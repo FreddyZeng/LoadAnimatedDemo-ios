3eca57da7e01aceabab894968584e027ce0b17ed
wenhuan <tigerAndBull@foxmail.com>
2021-06-06 16:48:22 +0800

    fix: delete controlAnimation

--- TABAnimatedDemo/TABAnimated/Control/UICollectionView+TABControlAnimation.m
+++ TABAnimatedDemo/TABAnimated/Control/UICollectionView+TABControlAnimation.m
@@ -6,28 +6,28 @@
 //  Copyright © 2021 tigerAndBull. All rights reserved.
 //
 
-#import "UICollectionView+TABControlAnimation.h"
-#import "TABCollectionAnimated.h"
-#import "UIView+TABControlModel.h"
-#import "UIView+TABControlAnimation.h"
-
-@implementation UICollectionView (TABControlAnimation)
-
-- (void)tab_startAnimationWithConfigBlock:(nullable TABCollectionViewConfigBlock)configBlock
-                              adjustBlock:(nullable TABAdjustBlock)adjustBlock
-                               completion:(nullable void (^)(void))completion {
-    if (!self.tabAnimated) {
-        TABCollectionAnimated *tabAnimated = TABCollectionAnimated.new;
-        tabAnimated.adjustBlock = adjustBlock;
-        self.tabAnimated = tabAnimated;
-    }
-    
-    if (configBlock && !self.tabAnimated.configed) {
-        configBlock(self.tabAnimated);
-        self.tabAnimated.configed = YES;
-    }
-    
-    [self tab_startAnimation];
-}
-
-@end
+//#import "UICollectionView+TABControlAnimation.h"
+//#import "TABCollectionAnimated.h"
+//#import "UIView+TABControlModel.h"
+//#import "UIView+TABControlAnimation.h"
+//
+//@implementation UICollectionView (TABControlAnimation)
+//
+//- (void)tab_startAnimationWithConfigBlock:(nullable TABCollectionViewConfigBlock)configBlock
+//                              adjustBlock:(nullable TABAdjustBlock)adjustBlock
+//                               completion:(nullable void (^)(void))completion {
+//    if (!self.tabAnimated) {
+//        TABCollectionAnimated *tabAnimated = TABCollectionAnimated.new;
+//        tabAnimated.adjustBlock = adjustBlock;
+//        self.tabAnimated = tabAnimated;
+//    }
+//
+//    if (configBlock && !self.tabAnimated.configed) {
+//        configBlock(self.tabAnimated);
+//        self.tabAnimated.configed = YES;
+//    }
+//
+//    [self tab_startAnimation];
+//}
+//
+//@end
--- TABAnimatedDemo/TABAnimated/Control/UITableView+TABControlAnimation.h
+++ TABAnimatedDemo/TABAnimated/Control/UITableView+TABControlAnimation.h
@@ -6,21 +6,21 @@
 //  Copyright © 2021 tigerAndBull. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "UIView+TABControlAnimation.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@class TABTableAnimated;
-
-typedef void(^TABTableViewConfigBlock)(TABTableAnimated * _Nonnull tabAnimated);
-
-@interface UITableView (TABControlAnimation)
-
-- (void)tab_startAnimationWithConfigBlock:(nullable TABTableViewConfigBlock)configBlock
-                              adjustBlock:(nullable TABAdjustBlock)adjustBlock
-                               completion:(nullable void (^)(void))completion;
-
-@end
-
-NS_ASSUME_NONNULL_END
+//#import <UIKit/UIKit.h>
+//#import "UIView+TABControlAnimation.h"
+//
+//NS_ASSUME_NONNULL_BEGIN
+//
+//@class TABTableAnimated;
+//
+//typedef void(^TABTableViewConfigBlock)(TABTableAnimated * _Nonnull tabAnimated);
+//
+//@interface UITableView (TABControlAnimation)
+//
+//- (void)tab_startAnimationWithConfigBlock:(nullable TABTableViewConfigBlock)configBlock
+//                              adjustBlock:(nullable TABAdjustBlock)adjustBlock
+//                               completion:(nullable void (^)(void))completion;
+//
+//@end
+//
+//NS_ASSUME_NONNULL_END
